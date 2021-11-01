f1448d4157f2d906a3525672ff634a5488171164
qweezique <qweezique@gmail.com>
2021-11-02 01:56:57 +0500

    init

---
+++ src/test/java/com/example/springbootdevpub/SpringBootDevPubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootdevpub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDevPubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
